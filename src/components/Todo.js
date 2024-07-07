4dc6e5d28e2b548c98f62107b060ad452681bfcf
Ceren Yılmaz <ceren.yilmaz@studenti.unipd.it>
2024-07-07 22:44:55 +0200

    todo-columns

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,7 +3,26 @@ import './Todo.css';
 
 function Todo() {
   return (
-    <div>Todo</div>
+    <div className='todo-container' draggable>
+      <div className='todo-column'> 
+        <div className='column-header'>
+          To-Do 
+          <hr/>
+        </div>
+      </div>
+      <div className='process-column'>
+        <div className='column-header'>
+          In Process
+          <hr/>
+        </div>
+      </div>
+      <div className='finished-column'>
+        <div className='column-header'>
+          Accomplished 
+          <hr/>
+        </div>
+      </div>
+    </div> 
   )
 }
 
