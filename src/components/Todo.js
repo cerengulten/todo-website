be21209a1bbc19e54dfdcb642aa3a7f9078e0695
Ceren Yılmaz <ceren.yilmaz@studenti.unipd.it>
2024-07-09 15:10:31 +0200

    styling for movable object

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,29 +1,44 @@
 import React from 'react';
+import { useState } from 'react';
 import './Todo.css';
+import { color } from '@mui/system';
 
 function Todo() {
+  const [cards, setCards] = useState();
   return (
-    <div className='todo-container' draggable>
-      <div className='todo-column'> 
-        <div className='column-header'>
-          To-Do 
-          <hr/>
-        </div>
-      </div>
-      <div className='process-column'>
-        <div className='column-header'>
-          In Process
-          <hr/>
-        </div>
-      </div>
-      <div className='finished-column'>
-        <div className='column-header'>
-          Accomplished 
-          <hr/>
-        </div>
-      </div>
+    <div className='todo-container'>
+      <Column 
+            title='To-Do'
+            column='todo'
+            headingColor='#073b4c'
+            cards={cards}
+            setCards={setCards}/>
+      <Column
+             title='Process'
+             column='process'
+             headingColor='#ffd166'
+             cards={cards}
+             setCards={setCards}/>
+      <Column  
+            title='Finished'
+            column='done'
+            headingColor='#06d6a0'
+            cards={cards}
+            setCards={setCards}/>
     </div> 
   )
 }
 
 export default Todo
+
+const Column = ({title, headingColor, cards, column, setCards}) =>{
+  return(
+    <div className='column-container'>
+      <div className='header-container'>
+        <h3 className='mainHeader' style={{color: headingColor}}>{title}</h3>
+        <span className='num-cards'>3</span> {/*number of the cards in the column will be in here*/}
+      </div>
+    </div>
+  );
+
+};
