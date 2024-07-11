9bf60532ac50c39bb489bb54d3376114cd7d7de5
Ceren Yılmaz <ceren.yilmaz@studenti.unipd.it>
2024-07-11 16:06:36 +0200

    cards-update

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { useState } from 'react';
 import './Todo.css';
-import { color } from '@mui/system';
 
 function Todo() {
-  const [cards, setCards] = useState();
+  const [cards, setCards] = useState(DEFAULT_CARDS);
   return (
     <div className='todo-container'>
       <Column 
@@ -32,13 +31,76 @@ function Todo() {
 export default Todo
 
 const Column = ({title, headingColor, cards, column, setCards}) =>{
+  const [active, setActive] = useState(false); /*used for hover the card, booelan */ 
+  const filteredCards = cards.filter((c) => c.column === column);
   return(
     <div className='column-container'>
       <div className='header-container'>
         <h3 className='mainHeader' style={{color: headingColor}}>{title}</h3>
-        <span className='num-cards'>3</span> {/*number of the cards in the column will be in here*/}
+        <span className='num-cards'>{filteredCards.length}</span> {/*number of the cards in the column will be in here*/}
+      </div>
+      <div className={`card ${active ? 'active' : ''}`}>
+        {filteredCards.map((c) => {
+          return <Card key={c.id} {...c}/>
+        })}
+        <DropIndicator beforeId="-1" column={column} />
       </div>
     </div>
   );
-
 };
+const Card = ({title, id, column}) =>{
+  return(
+    <div>
+      <DropIndicator beforeId={id} column={column}/>
+      <div 
+          layout
+          layoutId = {id}
+          draggable = 'true'
+          className='card-container'>
+          <p className='card-content'>{title}</p>
+      </div>
+      
+    </div>
+    
+ 
+  );
+};
+
+const DropIndicator = ({beforeId, column}) =>{
+  return(
+    <div 
+        data-before={beforeId || "-1"}
+        data-column={column}
+        className='drop-indicator'  />
+  );
+  };
+
+const DEFAULT_CARDS = [
+  // BACKLOG
+  { title: "Look into render bug in dashboard", id: "1", column: "todo" },
+  { title: "SOX compliance checklist", id: "2", column: "todo" },
+  { title: "[SPIKE] Migrate to Azure", id: "3", column: "todo" },
+  { title: "Document Notifications service", id: "4", column: "todo" },
+  // TODO
+  {
+    title: "Research DB options for new microservice",
+    id: "5",
+    column: "todo",
+  },
+  { title: "Postmortem for outage", id: "6", column: "todo" },
+  { title: "Sync with product on Q3 roadmap", id: "7", column: "todo" },
+
+  // DOING
+  {
+    title: "Refactor context providers to use Zustand",
+    id: "8",
+    column: "process",
+  },
+  { title: "Add logging to daily CRON", id: "9", column: "process" },
+  // DONE
+  {
+    title: "Set up DD dashboards for Lambda listener",
+    id: "10",
+    column: "done",
+  },
+];
